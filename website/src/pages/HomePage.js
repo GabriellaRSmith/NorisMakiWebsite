6e1ca44be6705214503c1e27b2469fc964cb0d08
chloekaii <116681534+chloekaii@users.noreply.github.com>
2025-11-06 13:44:47 -0500

    Made header and project structure

---
+++ website/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main className="home-page">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is placeholder text for now.</p>
+      <p>Additional sections will go here (shop, about, join, etc.).</p>
+    </main>
+  );
+}
+
+export default Home;
