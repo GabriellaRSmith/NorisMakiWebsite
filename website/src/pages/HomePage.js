86c957a7028dc7ca975245fe8b038537004208fc
Gabriella Smith <msgabyrsmith@gmail.com>
2025-11-07 14:23:49 -0500

    Created about section basic

--- website/src/pages/HomePage.js
+++ website/src/pages/HomePage.js
@@ -6,6 +6,15 @@ function Home() {
       <h1>Welcome to the Home Page</h1>
       <p>This is placeholder text for now.</p>
       <p>Additional sections will go here (shop, about, join, etc.).</p>
+      <h1>First we'll have the video here</h1>
+
+      <section className="about">
+        <h2 className="aboutTitle">Message from Noris Maki</h2>
+        <p className="aboutText">
+          "This album has been near and dear to my heart. I really focused my craft and am proud of what I've created. 
+          <em> Broken Heart Club</em>, has been my focus for the last year combining pop and ballads. I hope you enjoy it as much I have enjoyed making it."
+        </p>
+      </section>
     </main>
   );
 }
