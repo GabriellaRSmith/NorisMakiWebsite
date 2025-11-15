78c1094c1f7bf7a9c823e4c2c82dc2a39938db14
Jesus D. Lopez Marti <jlopezmarti20@gmail.com>
2025-11-15 12:55:05 -0500

    Update cart layout with additional comments and refinements

--- website/src/pages/CartPage.js
+++ website/src/pages/CartPage.js
@@ -1,15 +1,9 @@
-// website/src/pages/CartPage.js
 import React from "react";
-import "../components/CartPage.css"; // or wherever your CSS lives
-import CartPageContent from "../components/CartPage"; // optional if you're splitting layout
+import "../components/CartPage.css";
+import CartPageContent from "../components/CartPage";
 
 const CartPage = () => {
-  // for now you can just render your layout component or placeholder
-  return (
-    <CartPageContent />
-    // or:
-    // <div>Cart page placeholder</div>
-  );
+  return <CartPageContent />;
 };
 
-export default CartPage; // 🔴 THIS LINE IS THE IMPORTANT PART
+export default CartPage;
